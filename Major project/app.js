fca3f4b9b1fd9c174c6a2b4030a0fd662f314ca9
Maanav Shah <126866160+Rabbitshah@users.noreply.github.com>
2024-06-30 19:50:31 +0530

    Doubt solved

--- Major project/app.js
+++ Major project/app.js
@@ -1,58 +1,132 @@
+// const express = require("express");
+// const app = express();
+// const mongoose = require("mongoose");
+// const Listing = require("./models/listing.js");
+// const path = require("path");
+
+
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+// main().then(() => {
+//     console.log("Connected");
+// }).catch(err => {
+//     console.log(err);
+// });
+
+// async function main(){
+//     await mongoose.connect(MONGO_URL);
+// };
+
+// app.set("view engine ","ejs");
+// app.set("views", path.join(__dirname, "views"));
+// app.use(express.urlencoded({extended: true}));
+
+// app.listen(8080, () =>{
+//     console.log("server is listening to 8080");
+// });
+
+// app.get('/' , (req , res)=>{
+//    res.send('hello im root');
+// });
+
+// // Index Route
+// app.get("/listings", async (req,res) => {
+//     const allListings = await Listing.find({})
+//     res.render("listings/index.ejs", {allListings});
+// });
+
+
+// // New route
+// app.get("/listings/new", (req,res) => {
+//     res.render("listings/new.ejs");
+// });
+
+// // Show Route
+// app.get("/listings/:id", async (req,res) => {
+//     let {id} = req.params;
+//     const listing = await Listing.findById(id);
+//     res.render("listings/show.ejs", {listing});
+// });
+
+// // Create Route
+// app.post("/listings", async (req,res) => {
+//     // let {title, description, image, price, location, country} = req.body;
+//     let {listing} =  req.body.listing;
+//     console.log(listing);
+// });
+
+// // app.get("/testListing", async (req,res) => {
+// //     let sampleListing = new Listing({
+// //         title: "My New Villa",
+// //         description: "By the beach",
+// //         price: 1200,
+// //         location: "Calangute Goa",
+// //         country: "India"
+// //     });
+// //     await sampleListing.save()
+// //     console.log("sample was saved");
+// //     res.send("successful testing");
+// // });
+
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 
-
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
-main().then(() => {
+main()
+  .then(() => {
     console.log("Connected");
-}).catch(err => {
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
-async function main(){
-    await mongoose.connect(MONGO_URL);
-};
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
 
-app.set("view engine ","ejs");
+app.set("view engine ", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(8080, () =>{
-    console.log("server is listening to 8080");
+app.listen(3000, () => {
+  console.log("server is listening to 3000");
 });
 
-app.get('/' , (req , res)=>{
-   res.send('hello im root');
+app.get("/", (req, res) => {
+  res.send("hello im root");
 });
 
 // Index Route
-app.get("/listings", async (req,res) => {
-    const allListings = await Listing.find({})
-    res.render("listings/index.ejs", {allListings});
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
 });
 
-
 // New route
-app.get("/listings/new", (req,res) => {
-    res.render("listings/new.ejs");
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
 });
 
 // Show Route
-app.get("/listings/:id", async (req,res) => {
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show.ejs", {listing});
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
 });
 
 // Create Route
-app.post("/listings", async (req,res) => {
-    // let {title, description, image, price, location, country} = req.body;
-    let {listing} =  req.body.listing;
-    console.log(listing);
+app.post("/listings", async (req, res) => {
+  let { listing } = req.body;
+
+  let newListing = await new Listing({ ...listing });
+  console.log(newListing);
+  newListing.save();
+  res.redirect("/listings");
 });
 
 // app.get("/testListing", async (req,res) => {
