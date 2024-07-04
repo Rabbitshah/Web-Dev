59ed461117e1d18507818530dfbb321d88bc1416
Maanav Shah <126866160+Rabbitshah@users.noreply.github.com>
2024-07-04 23:18:23 +0530

    Finished Marjor project part 1(phase a)

--- Major project/app.js
+++ Major project/app.js
@@ -74,6 +74,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -92,6 +93,7 @@ async function main() {
 app.set("view engine ", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.listen(8080, () => {
   console.log("server is listening to 3000");
@@ -129,15 +131,24 @@ app.post("/listings", async (req, res) => {
   res.redirect("/listings");
 });
 
-// app.get("/testListing", async (req,res) => {
-//     let sampleListing = new Listing({
-//         title: "My New Villa",
-//         description: "By the beach",
-//         price: 1200,
-//         location: "Calangute Goa",
-//         country: "India"
-//     });
-//     await sampleListing.save()
-//     console.log("sample was saved");
-//     res.send("successful testing");
-// });
+// Edit Route
+app.get('/listings/:id/edit' , async (req , res)=>{
+  let { id } = req.params;
+  let listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", {listing});
+});
+
+// Update Route
+app.put('/listings/:id' , async (req , res)=>{
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id , {...req.body.listing});
+  res.redirect(`/listings/${id}`);
+});
+
+// Delete Route
+app.delete('/listings/:id' , async (req , res)=>{
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect('/listings');
+});
