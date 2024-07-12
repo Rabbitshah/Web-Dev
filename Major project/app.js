64e78508d1ef889c50d1357c1ba4a44997e22393
Maanav <maanavshah09@gmail.com>
2024-07-12 23:41:39 +0530

    Style ke liye prep

--- Major project/app.js
+++ Major project/app.js
@@ -1,80 +1,10 @@
-// const express = require("express");
-// const app = express();
-// const mongoose = require("mongoose");
-// const Listing = require("./models/listing.js");
-// const path = require("path");
-
-
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-
-// main().then(() => {
-//     console.log("Connected");
-// }).catch(err => {
-//     console.log(err);
-// });
-
-// async function main(){
-//     await mongoose.connect(MONGO_URL);
-// };
-
-// app.set("view engine ","ejs");
-// app.set("views", path.join(__dirname, "views"));
-// app.use(express.urlencoded({extended: true}));
-
-// app.listen(8080, () =>{
-//     console.log("server is listening to 8080");
-// });
-
-// app.get('/' , (req , res)=>{
-//    res.send('hello im root');
-// });
-
-// // Index Route
-// app.get("/listings", async (req,res) => {
-//     const allListings = await Listing.find({})
-//     res.render("listings/index.ejs", {allListings});
-// });
-
-
-// // New route
-// app.get("/listings/new", (req,res) => {
-//     res.render("listings/new.ejs");
-// });
-
-// // Show Route
-// app.get("/listings/:id", async (req,res) => {
-//     let {id} = req.params;
-//     const listing = await Listing.findById(id);
-//     res.render("listings/show.ejs", {listing});
-// });
-
-// // Create Route
-// app.post("/listings", async (req,res) => {
-//     // let {title, description, image, price, location, country} = req.body;
-//     let {listing} =  req.body.listing;
-//     console.log(listing);
-// });
-
-// // app.get("/testListing", async (req,res) => {
-// //     let sampleListing = new Listing({
-// //         title: "My New Villa",
-// //         description: "By the beach",
-// //         price: 1200,
-// //         location: "Calangute Goa",
-// //         country: "India"
-// //     });
-// //     await sampleListing.save()
-// //     console.log("sample was saved");
-// //     res.send("successful testing");
-// // });
-
-
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -94,6 +24,9 @@ app.set("view engine ", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,"/public")))
+
 
 app.listen(8080, () => {
   console.log("server is listening to 3000");
