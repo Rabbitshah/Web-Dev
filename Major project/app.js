a1a7210963d6d978892dbe2b33535ee36ca300fc
Maanav <maanavshah09@gmail.com>
2024-07-22 23:43:47 +0530

    Style.css connected

--- Major project/app.js
+++ Major project/app.js
@@ -21,11 +21,11 @@ async function main() {
 }
 
 app.set("view engine ", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "/views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname,"/public")))
+app.use(express.static(path.join(__dirname,"/public/css")));
 
 
 app.listen(8080, () => {
