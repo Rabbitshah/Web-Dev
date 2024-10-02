3da499dacf0d8770d33180b067b86da48aaeba78
Maanav Shah <devp04412@gmail.com>
2024-10-03 01:28:32 +0530

    Database relationships over

---
+++ Relationships/Models/user.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+main().then(() => console.log("connection successfull")).catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/relationdemo');
+}
+
+const userSchema = new Schema({
+  username : String,
+  addresses: [
+    {
+      _id: false,
+      location: String,
+      city: String,
+  },
+],
+});
+
+const User = mongoose.model("User", userSchema);
+
+const addUser = async() => {
+  let user1 = new User({
+    username: "Sherlockhomes",
+    addresses: [
+      {
+        location: "221B Baker Street",
+        city: "London",
+    },
+  ],
+  });
+
+  user1.addresses.push({location:"P32 WallStreet", city:"London"});
+  let result = await user1.save();
+  console.log(result);
+};
+
+addUser();
